4a2ce3f1a2fbb8aca3415979586b40de8ccd677e
peter <petecruckshank@gmail.com>
2018-08-27 22:10:23 -0400

    added .drum-pad with <audio> and <p> tags

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,11 +6,46 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          
         </header>
         <div id="display">
-          ${ /* the area with the 9 .drum-pads each with <audio> elms */ }
+          { /* the area with the 9 .drum-pads each with <audio> elms */ }
+          <div className="drum-pad">
+            <p>Q</p>
+            <audio src=""></audio>
+          </div>
+          <div className="drum-pad">
+          <p>W</p>
+            <audio src=""></audio>
+          </div>
+          <div className="drum-pad">
+          <p>E</p>
+            <audio src=""></audio>
+          </div>
+          <div className="drum-pad">
+          <p>A</p>
+            <audio src=""></audio>
+          </div>
+          <div className="drum-pad">
+          <p>S</p>
+            <audio src=""></audio>
+          </div>
+          <div className="drum-pad">
+          <p>D</p>
+            <audio src=""></audio>
+          </div>
+          <div className="drum-pad">
+          <p>Z</p>
+            <audio src=""></audio>
+          </div>
+          <div className="drum-pad">
+          <p>X</p>
+            <audio src=""></audio>
+          </div>
+          <div className="drum-pad">
+          <p>C</p>
+            <audio src=""></audio>
+          </div>
         </div>
       </div>
     );
